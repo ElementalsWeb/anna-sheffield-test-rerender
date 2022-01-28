fba1608b6e239eb4976fc0c53c3cca911c6d18f7
Andriy Lobatskiy <technical@elementalsweb.com>
2022-01-28 13:57:47 +0200

    update

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
 import path from "path";
 import express from "express";
-import fs from "fs";
-import cors from "cors";
-import fetch from "node-fetch"; 
-import shopify from "shopify-buy";
-import getPriceRings from "./api/index.js";
-(global as any).fetch = fetch;
+// import fs from "fs";
+// import cors from "cors";
+// import fetch from "node-fetch"; 
+// import shopify from "shopify-buy";
+// import getPriceRings from "./api/index.js";
+// (global as any).fetch = fetch;
 
 const app = express();
 const __dirname = path.resolve();
@@ -14,52 +14,52 @@ let PORT: any = process.env.PORT || 3001;
 if (process.env.NODE_ENV === "development") {
   PORT = process.env.DEV_PORT_API;
 }
-// Middlewares
-app.use(cors());
-app.use("/", async (req, res, next) => {
-  // console.log('werwer');
+// // Middlewares
+// app.use(cors());
+// app.use("/", async (req, res, next) => {
+//   // console.log('werwer');
 
-  const queryParams: any = req.query;
+//   const queryParams: any = req.query;
 
-  if (
-    queryParams["update_pricing"] &&
-    queryParams["update_pricing"] === "true"
-  ) {
+//   if (
+//     queryParams["update_pricing"] &&
+//     queryParams["update_pricing"] === "true"
+//   ) {
 
-    const client: any = shopify.buildClient(
-      {
-        domain: "annasheffield.myshopify.com",
-        storefrontAccessToken: "6ce2afdc8e2e91cf7c8c3ebcffb3c629",
-      },
-    ); 
-    let priceRings: any = await getPriceRings(client); 
-    const filePath: any = path.resolve(__dirname, "dist", "file", "price.json");
-    const dataWriteFile: any = JSON.stringify(priceRings);
+//     const client: any = shopify.buildClient(
+//       {
+//         domain: "annasheffield.myshopify.com",
+//         storefrontAccessToken: "6ce2afdc8e2e91cf7c8c3ebcffb3c629",
+//       },
+//     ); 
+//     let priceRings: any = await getPriceRings(client); 
+//     const filePath: any = path.resolve(__dirname, "dist", "file", "price.json");
+//     const dataWriteFile: any = JSON.stringify(priceRings);
 
-    try {
-      fs.writeFileSync(filePath, dataWriteFile);
-      return res.json({ 'status': 'ok' });
+//     try {
+//       fs.writeFileSync(filePath, dataWriteFile);
+//       return res.json({ 'status': 'ok' });
 
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  next();
-});
-app.get("/getPrices", async function (req, res, next) {
-  const filePath: any = path.resolve(__dirname, "dist", "file", "price.json");
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+//   next();
+// });
+// app.get("/getPrices", async function (req, res, next) {
+//   const filePath: any = path.resolve(__dirname, "dist", "file", "price.json");
 
-  const data: any = await fs.readFileSync(filePath, {
-    encoding: "utf8",
-    flag: "r",
-  });
+//   const data: any = await fs.readFileSync(filePath, {
+//     encoding: "utf8",
+//     flag: "r",
+//   });
 
-  return res.json(JSON.parse(data));
-});
+//   return res.json(JSON.parse(data));
+// });
 
 // ROUTES
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
